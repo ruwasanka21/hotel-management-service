ede56f2d72ab0b79eb47b4866812c9491ed68c24
ruwasanka21 <ruwasanka21@gmail.com>
2025-08-21 13:36:13 +0530

    Commit 3

--- hotel-management-service-api/src/main/java/com/cpd/hotel_system/hotel_management_service_api/HotelManagementServiceApiApplication.java
+++ hotel-management-service-api/src/main/java/com/cpd/hotel_system/hotel_management_service_api/HotelManagementServiceApiApplication.java
@@ -2,8 +2,10 @@ package com.cpd.hotel_system.hotel_management_service_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HotelManagementServiceApiApplication {
 
 	public static void main(String[] args) {
